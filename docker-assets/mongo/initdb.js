aba9726e6c5679c978bebe1acc9904fa6484930a
Lehcode <53556648+lehcode@users.noreply.github.com>
2023-04-25 01:24:36 +0300

    Mongo insecure

--- docker-assets/mongo/initdb.js
+++ docker-assets/mongo/initdb.js
@@ -4,22 +4,25 @@ console.log(process.env);
 const conn = new Mongo();
 
 // Initialize constants
-const collections = process.env.collections.split(",");
-const rootUser = process.env.root_user;
-const rootPass = process.env.root_pass;
+const collections = process.env.mongo_collections.split(",");
+const rootUser = process.env.mongo_root_user;
+const rootPass = process.env.mongo_root_pass;
 const adminDb = conn.getDB('admin');
 const dataDb = conn.getDB(process.env.db);
-const userName = process.env.user;
-const userPass = process.env.pass;
+const userName = process.env.mongo_user;
+const userPass = process.env.mongo_pass;
 
 adminDb.createUser({ user: rootUser, pwd: rootPass, roles: ["root"] });
-adminDb.auth(rootUser, rootPass);
 
 // Create the user with read and write access to the specified collections
 adminDb.createUser({
   user: userName,
   pwd: userPass,
-  roles: [{ role: "readWrite", db: process.env.db }]
+  roles: collections.map(name => ({
+    role: "readWrite",
+    db: process.env.db,
+    collection: name.trim()
+  }))
 });
 
 adminDb.auth(userName, userPass);
