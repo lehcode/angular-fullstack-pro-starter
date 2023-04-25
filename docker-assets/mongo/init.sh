74e2ca89bfcb7a39efc471d4c24d276fdcae52dd
Lehcode <53556648+lehcode@users.noreply.github.com>
2023-04-25 22:51:46 +0300

    Added OpenSSL service andd certificates generation

--- docker-assets/mongo/init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-[[ ${DEBUG} = "yes" ]] && set -ex || set -e
-[[ ${DEBUG} = "yes" ]] && env
-
-mongod --auth --nounixsocket \
-    --bind_ip ${IP} \
-    --port ${PORT}
-    --wiredTigerCacheSizeGB ${CACHE_SIZE_GB}
