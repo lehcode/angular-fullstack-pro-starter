46211e235810aa68bdff3ebc2d9ce158dd83335e
Antony Repin <53556648+lehcode@users.noreply.github.com>
2023-04-26 05:49:26 +0300

    Feature/mongo secure (#4)
    * Mongo insecure
    
    * Added V3 extension file and certificate generation description
    
    * Added OpenSSL service andd certificates generation
    
    * Mongo TLS enabled

---
+++ docker-assets/mongo/initdb.js
@@ -0,0 +1,29 @@
+console.log(process.env);
+
+// Connect to the MongoDB instance
+const conn = new Mongo();
+
+// Initialize constants
+const collections = process.env.mongo_collections.split(",");
+const rootUser = process.env.mongo_root_user;
+const rootPass = process.env.mongo_root_pass;
+const dataDbName = process.env.db;
+const adminDb = conn.getDB('admin');
+const dataDb = conn.getDB(dataDbName);
+const userName = process.env.mongo_user;
+const userPass = process.env.mongo_pass;
+
+adminDb.createUser({ user: rootUser, pwd: rootPass, roles: ["root"] });
+
+// Create the user with read and write access to the specified collections
+adminDb.createUser({
+  user: userName,
+  pwd: userPass,
+  roles: collections.map(name => ({
+    role: "readWrite",
+    db: dataDbName,
+    collection: name.trim()
+  }))
+});
+
+adminDb.auth(userName, userPass);
