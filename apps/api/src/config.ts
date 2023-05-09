26aa15c82e6e3d1107b77bf7fb899e4edef52f0a
Lehcode <53556648+lehcode@users.noreply.github.com>
2023-05-09 03:57:43 +0300

    Fixed MongoDB connection

--- apps/api/src/config.ts
+++ apps/api/src/config.ts
@@ -1,7 +1,13 @@
 export default (): Record<string, any> => ({
   api: {
-    port: process.env.API_PORT,
-    ssl: process.env.SSL
+    port: process.env.API_PORT || 3000,
+    ssl: true
+  },
+  auth: {
+    passport: {
+      // More Passport strategies at http://www.passportjs.org/packages/
+      strategy: 'local'
+    }
   },
   hostname: process.env.HOSTNAME || 'localhost',
   services: {},
@@ -13,11 +19,21 @@ export default (): Record<string, any> => ({
     defaultNs: 'default'
   },
   mongo: {
-    host: process.env.MONGO_HOST,
-    port: process.env.MONGO_PORT,
+    host: process.env.MONGO_HOST || 'localhost',
+    port: process.env.MONGO_PORT_CONTAINER || 27017,
     urlParams: process.env.MONGO_URL_PARAMS,
-    db: process.env.MONGO_DB,
+    db: process.env.MONGO_DB || 'starter',
     user: process.env.MONGO_USER,
-    pass: process.env.MONGO_PASS
+    pass: process.env.MONGO_PASS,
+    secure: process.env.MONGO_SECURE
+
+  },
+  mysql: {
+    host: process.env.MYSQL_HOST || 'localhost',
+    port: process.env.MYSQL_PORT || 3306,
+    db: process.env.MYSQL_DB || 'starter',
+    user: process.env.MYSQL_USER,
+    pass: process.env.MYSQL_PASS
   }
 });
+// @ts-ignore
